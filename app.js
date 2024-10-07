787b559745172625b32188f55518bb2c88df841c
abikel1 <abikel@g.jct.ac.il>
2024-10-07 14:21:16 +0300

    try

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("pages/index");
 });
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
