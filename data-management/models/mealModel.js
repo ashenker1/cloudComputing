9032fdeeb9623fb93023a82ff0761764d173cac0
ashenker <ashenker@g.jct.ac.il>
2024-10-08 20:46:43 +0300

    meal db changes

--- data-management/models/mealModel.js
+++ data-management/models/mealModel.js
@@ -1,39 +1,42 @@
-const db = require('../config');
+const db = require("../config");
 
 // פונקציה ליצירת ארוחה חדשה
-const createMeal = async (meal) => {
+const createMeal = async (meal, userId) => {
   return db.query(
-    `INSERT INTO Meals (name, description, date, imageUrl, sugarLevel, isHoliday) 
-    VALUES (@name, @description, @date, @imageUrl, @sugarLevel, @isHoliday)`, 
-    meal
+    `INSERT INTO Meals (name, description, date, imageUrl, sugarLevel, isHoliday, userId) 
+    VALUES (@name, @description, @date, @imageUrl, @sugarLevel, @isHoliday, @userId)`,
+    { ...meal, userId }
   );
 };
 
 // פונקציה לקבלת ארוחה לפי מזהה
 const getMealById = async (mealId) => {
-  return db.query('SELECT * FROM Meals WHERE id = @mealId', { mealId });
+  return db.query("SELECT * FROM Meals WHERE id = @mealId", { mealId });
 };
 
 // פונקציה לקבלת היסטורית ארוחות לפי טווח תאריכים
 const getMealsByDateRange = async (startDate, endDate) => {
-  return db.query('SELECT * FROM Meals WHERE date BETWEEN @startDate AND @endDate', {
-    startDate,
-    endDate,
-  });
+  return db.query(
+    "SELECT * FROM Meals WHERE date BETWEEN @startDate AND @endDate",
+    {
+      startDate,
+      endDate,
+    }
+  );
 };
 
 // פונקציה לעדכון ארוחה
-const updateMeal = async (mealId, meal) => {
+const updateMeal = async (mealId, meal, userId) => {
   return db.query(
     `UPDATE Meals SET name = @name, description = @description, date = @date, 
-    imageUrl = @imageUrl, sugarLevel = @sugarLevel, isHoliday = @isHoliday WHERE id = @mealId`, 
-    { ...meal, mealId }
+    imageUrl = @imageUrl, sugarLevel = @sugarLevel, isHoliday = @isHoliday, userId = @userId WHERE id = @mealId`,
+    { ...meal, userId, mealId }
   );
 };
 
 // פונקציה למחיקת ארוחה
 const deleteMeal = async (mealId) => {
-  return db.query('DELETE FROM Meals WHERE id = @mealId', { mealId });
+  return db.query("DELETE FROM Meals WHERE id = @mealId", { mealId });
 };
 
 module.exports = {
